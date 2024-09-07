26853c730a5715700daa6c8b3883ba176f8ea28a
vizdxm <fahtai.tan@gmail.com>
2024-09-07 22:10:57 +0700

    wait, this is real

--- runthiscode.cpp
+++ runthiscode.cpp
@@ -25,7 +25,7 @@ int main () {
   vector <string> id; //id.push_back("dummy");
   string des;
   cout << "user manual:\n1. Open command prompt and select original file folder by typing 'cd' and the path to the folder\n2. Create a folder that will be the destination for the selected files in the original folder\n3. Then enter the desination folder name and the file IDs ending with a 0 in this program. \n4. Finally copy the output of this program to paste in the command prompt, enter and just wait for the files to be copied to the destination folder.\nnote : this program alse removes duplicate IDs and sorts the IDs in ascending order.\n\n";
-  cout << "Ex: selecting wedding photo number 2 89 40 and 600 for editing\n(in CMD prompt) cd C:\\documents\\photos\\wedding_photos\n(in wedding_photos folder) create new folder -  for_editing\n(run this code and enter) for_editing 2 89 40 600 0\n(in CMD prompt) paste the text generated from this code and done\n\n\n";
+  cout << "Ex: selecting wedding photo number 2 89 40 and 600 for editing\n(in CMD prompt) cd C:\\documents\\photos\\wedding_photos\n(in wedding_photos folder) create new folder -  for_editing\n(run this code and enter) canonR6___, .jpg, for_editing, 2 89 40 600 0\n(in CMD prompt) paste the text generated from this code and done\n\n\n";
   
   cout << "enter file heading (ex: CanonR6__) : ";
   cin >> heading;
